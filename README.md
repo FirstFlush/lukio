1f18b4fa2127846bd0add6d1a00ebc54a3cc74e7
FirstFlush <firstflush@protonmail.com>
2024-09-04 21:04:18 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Lukio
+
+**Lukio: A Simple Tool for Mounting LUKS Volumes**
+
+Lukio is a lightweight command-line script for mounting and unmounting LUKS encrypted volumes using various backends. Currently it supports zuluCrypt, cryptsetup, and udisksctl.
+
+## Features
+
+- Mount and unmount LUKS encrypted volumes with different backends.
+- Currently supports zuluCrypt, cryptsetup, and udisksctl.
+
+## Installation
+
+Clone the repository and run the installation script:
+
+```bash
+git clone https://github.com/FirstFlush/lukio.git
+bash install.sh
+```
---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+TARGET_DIR="/usr/local/bin"
+SCRIPT="bin/lukio"
+
+# Check if the script exists
+if [ -f "$SCRIPT" ]; then
+    echo "Installing Lukio to $TARGET_DIR..."
+    # Copy the script to the target directory
+    sudo cp "$SCRIPT" "$TARGET_DIR/lukio"
+    # Make the script executable
+    sudo chmod +x "$TARGET_DIR/lukio"
+    echo "Installation complete. You can now run 'lukio' from anywhere."
+else
+    echo "Error: Script $SCRIPT not found. Please ensure you run the install script from the project root directory."
+    exit 1
+fi
